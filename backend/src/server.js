f14e21053a58e65965b14ecd285d4edd04135b3c
azadgupta1 <azad236820@gmail.com>
2024-12-26 18:01:56 +0530

    Initial commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,15 @@
-const PORT = 5000;
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send("Welcome to LearnMate!");
+})
+
+app.listen(PORT, () =>{
+    console.log("Server is running on port "+PORT);
+})
