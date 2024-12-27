b125828a89f10176d00f40a49b3fecc8be9c32dc
azadgupta1 <azad236820@gmail.com>
2024-12-27 15:30:22 +0530

    Initial commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,8 +8,8 @@ const app = express();
 
 app.get('/', (req, res) =>{
     res.send("Welcome to LearnMate!");
-})
+});
 
 app.listen(PORT, () =>{
     console.log("Server is running on port "+PORT);
-})
+});
