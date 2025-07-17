fd9537f26499f5db8f465fac4979de95a2f86852
azadgupta1 <azad236820@gmail.com>
2025-07-17 19:59:39 +0530

    Updating the MetaData

--- frontend/src/services/authServices.js
+++ frontend/src/services/authServices.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
-export const API_URL = "https://nuvora-backend-fa1q.onrender.com"; // Change based on your backend route
+// export const API_URL = "https://nuvora-backend-fa1q.onrender.com"; // Change based on your backend route
+
+export const API_URL = "http://localhost:3000";
 
 export const registerUser = async (userData) => {
   try {
-    const response = await axios.post(`${API_URL}/signup`, userData);
+    const response = await axios.post(`${API_URL}/api/auth/signup`, userData);
     return response.data;
   } catch (error) {
     throw error.response?.data || { message: "Something went wrong!" };
@@ -13,7 +15,7 @@ export const registerUser = async (userData) => {
 
 export const loginUser = async (userData) => {
   try {
-    const response = await axios.post(`${API_URL}/login`, userData);
+    const response = await axios.post(`${API_URL}/api/auth/login`, userData);
     return response.data;
   } catch (error) {
     throw error.response?.data || { message: "Something went wrong!" };
