6aa092a9183aa85d3e105833f96911f50e496017
azadgupta1 <azad236820@gmail.com>
2025-09-05 14:53:50 +0530

    Testing frontend deployment

--- frontend/src/services/authServices.js
+++ frontend/src/services/authServices.js
@@ -2,11 +2,14 @@ import axios from "axios";
 
 // export const API_URL = "https://nuvora-backend-fa1q.onrender.com"; // Change based on your backend route
 
-export const API_URL = "http://localhost:3000";
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+
+// export const API_URL = "http://localhost:3000";
 
 export const registerUser = async (userData) => {
   try {
-    const response = await axios.post(`${API_URL}/api/auth/signup`, userData);
+    const response = await axios.post(`${backendUrl}/api/auth/signup`, userData);
     return response.data;
   } catch (error) {
     throw error.response?.data || { message: "Something went wrong!" };
@@ -15,7 +18,7 @@ export const registerUser = async (userData) => {
 
 export const loginUser = async (userData) => {
   try {
-    const response = await axios.post(`${API_URL}/api/auth/login`, userData);
+    const response = await axios.post(`${backendUrl}/api/auth/login`, userData);
     return response.data;
   } catch (error) {
     throw error.response?.data || { message: "Something went wrong!" };
